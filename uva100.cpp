c50016b206e055d3f493d2eb5fa9dea0956043ea
tim70036 <tim70036@gmail.com>
2017-09-22 01:34:04 +0800

    Uva u fucking little piece of shit

--- uva100.cpp
+++ uva100.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-
+#include <algorithm>
+#define N 10000000
 using namespace std;
 
-int ans[10005];
+int ans[N];
 
 void run(int n)
 {
@@ -20,11 +21,14 @@ void run(int n)
 int main()
 {
     int a , b;
-    for(int i=0 ; i<10005 ; i++) ans[i] = -1;
-    while(cin >> a >> b)
+    for(int i=0 ; i<N ; i++) ans[i] = -1;
+    while(scanf("%d %d",&a,&b) != EOF)
     {
+        int from = a, to = b;
+        if(a > b) swap(from, to);
+
         int max = -1;
-        for(int i=a ; i<=b ; i++)
+        for(int i=from ; i<=to ; i++)
         {
             if(ans[i] == -1)
                 run(i);
